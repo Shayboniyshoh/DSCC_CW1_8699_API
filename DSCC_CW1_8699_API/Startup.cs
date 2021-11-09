e3a877928f5ac13c38a4c43da46262e269817c6c
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-11-09 19:34:05 +0500

    swagger enabled for the production

--- DSCC_CW1_8699_API/Startup.cs
+++ DSCC_CW1_8699_API/Startup.cs
@@ -42,7 +42,7 @@ namespace DSCC_CW1_8699_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
